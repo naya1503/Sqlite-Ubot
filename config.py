08608dc2d039e1b1ee1bc1d54105faf9123d0f6b
Lucifer <38715155+jonesroot@users.noreply.github.com>
2023-11-24 20:35:26 +0700

    fix some bugs on config

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 
-from dotenv import load_dotenv()
+from dotenv import load_dotenv
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+load_dotenv()
+
+API_ID = int(os.getenv("API_ID", ""))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 SHARELOCK = os.getenv("SHARELOCK", "nandev.db")
 SESSION = os.getenv("SESSION", "")
 OPENAI_KEY = os.getenv("OPENAI_KEY", "")
